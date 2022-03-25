c9511194540563f15c74b7f6ef35a5a5ea862e26
pedro.ferreira@findmore.eu <pedro.ferreira@findmore.eu>
2022-03-25 09:58:12 +0000

    first commit

---
+++ movies.js
@@ -0,0 +1,30 @@
+const movies = require('./data.js');
+
+// Iteration 1: All directors? - Get the array of all directors.
+// _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
+// How could you "clean" a bit this array and make it unified (without duplicates)?
+const getAllDirectors = () => {};
+
+// Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
+const getNumberOfDramaMovies = () => {};
+
+// Iteration 3: All scores average - Get the average of all scores with 2 decimals
+const getAverageScore = () => {};
+
+// Iteration 4: Drama movies - Get the average of Drama Movies
+const getAverageDramaScore = () => {};
+
+// Iteration 5: Ordering by year - Order by year, ascending (in growing order)
+const getMoviesByYear = () => {};
+
+// Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
+const getMoviesByTitle = () => {};
+
+// BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
+const turnHoursToMinutes = () => {};
+
+// BONUS - Iteration 8: Best yearly score average - Best yearly score average
+const getBestYearlyScoreAverage = () => {};
+
+// BONUS - Iteration 9: Worst yearly score average - Worst yearly score average
+const getWorstYearlyScoreAverage = () => {};
