b5e516778c6c02d61d43edf96d93e2cd9150bf46
Leonardo <l7pereira7@gmail.com>
2022-03-30 11:00:45 +0100

    Done movies.js

--- movies.js
+++ movies.js
@@ -73,20 +73,107 @@ const getAverageDramaScore = () => {
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 const getMoviesByYear = () => {
   const sorted = movies.sort((movieOne, movieTwo) => {
-    return movieOne.year - movieTwo.year;
+    const { title: titleOne, year: yearOne } = movieOne;
+    const { title: titleTwo, year: yearTwo } = movieTwo;
+    if (yearOne - yearTwo === 0) {
+      return titleOne.localeCompare(titleTwo);
+    } else {
+      return yearOne - yearTwo;
+    }
   });
   return sorted;
 };
-console.log(getMoviesByYear());
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-const getMoviesByTitle = () => {};
+const getMoviesByTitle = () => {
+  const sorte = movies
+    .sort((movieOne, movieTwo) => {
+      return movieOne.title.localeCompare(movieTwo.title);
+    })
+    .slice(0, 20);
+  return sorte;
+};
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-const turnHoursToMinutes = () => {};
+const turnHoursToMinutes = () => {
+  const newMovies = movies.map((movie) => {
+    const { duration } = movie;
+    let durationSplit = duration.split(" ");
+    let hour = parseInt(durationSplit[0][0]);
+    let minutes = durationSplit[1];
+    if (!minutes) {
+      minutes = hour * 60;
+    } else {
+      for (let i = 0; i < minutes.length; i++) {
+        if (minutes[i++] === "m") {
+          firstMinute = parseInt(minutes[i]);
+          return `${firstMinute}`;
+        } else {
+          firstMinute = parseInt(minutes[i]);
+          secondMinute = parseInt(minutes[i++]);
+          return `${firstMinute} ${secondMinute}`;
+        }
+      }
+    }
+  });
+  return newMovies;
+};
 
-// BONUS - Iteration 8: Best yearly score average - Best yearly score average
-const getBestYearlyScoreAverage = () => {};
+// BONUS - Iteration 8: Best yearly score - Best yearly score average
+const getBestYearlyScoreAverage = () => {
+  let usedYearsArray = [];
+  let highestAverage = 0;
+  const finalObj = {};
+  movies.map((movie) => {
+    if (usedYearsArray.includes(movie.year)) {
+      return;
+    } else {
+      usedYearsArray.push(movie.year);
+      let filtered = movies.filter((movieFilter) => {
+        return movieFilter.year === movie.year;
+      });
+      const yearAverage = filtered.reduce((acc, movie) => acc + movie.score, 0);
+      let average = yearAverage / filtered.length;
+      if (average >= highestAverage) {
+        highestAverage = average;
 
-// BONUS - Iteration 9: Worst yearly score average - Worst yearly score average
-const getWorstYearlyScoreAverage = () => {};
+        return (immutableObj = {
+          ...finalObj,
+          highestAverage,
+          year: movie.year,
+        });
+      }
+    }
+  });
+  return immutableObj;
+};
+
+// BONUS - Iteration 9: Worst yearly score - Worst yearly score average
+const getWorstYearlyScoreAverage = () => {
+  let usedYearsArray = [];
+  let lowestAverage = 10;
+  const finalObj = {};
+  movies.map((movie) => {
+    if (usedYearsArray.includes(movie.year)) {
+      return;
+    } else {
+      usedYearsArray.push(movie.year);
+      let filtered = movies.filter((movieFilter) => {
+        return movieFilter.year === movie.year;
+      });
+      const yearAverage = filtered.reduce((acc, movie) => acc + movie.score, 0);
+      let average = yearAverage / filtered.length;
+      if (average <= lowestAverage) {
+        lowestAverage = average;
+
+        return (immutableObj = {
+          ...finalObj,
+          lowestAverage,
+          year: movie.year,
+        });
+      }
+    }
+  });
+  return immutableObj;
+};
+console.log(getWorstYearlyScoreAverage());
